9b4ff55887a7554e22320f9f45db3671e3e40a1a
Jeremy Okamoto <okamotojeremy@gmail.com>
2021-08-02 10:57:57 -0700

    a

---
+++ file.py
@@ -0,0 +1,27 @@
+
+"""for x in range(0, 151, 1):
+    print (x)"""
+
+"""for x in range(5, 1001, 5):
+    print(x)"""
+"""for x in range(1, 101, 1):
+    if x%10 == 0:
+        print ("Coding Dojo")
+    elif x%5 == 0:
+        print ("Coding")
+    else:
+        print (x)"""
+"""totalSum = 0
+for x in range(0, 500001, 1):
+   if x % 2 != 0:
+    totalSum = totalSum + x
+   
+print(totalSum)"""
+"""for x in range(2018, 0, -4):
+    print(x)"""
+lowNum = 2
+hightNum = 9
+mult = 3
+for x in range(lowNum, hightNum+1, 1):
+    if x%mult == 0:
+        print(x)
